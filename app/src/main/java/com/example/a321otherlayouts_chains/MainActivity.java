727c9fb2b7d68683e376ee16890c125abcc0de63
denlog1977 <denlog1977@gmail.com>
2019-10-29 14:43:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a321otherlayouts_chains/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a321otherlayouts_chains;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
